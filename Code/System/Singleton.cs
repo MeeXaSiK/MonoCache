10ef3e89a135c2d9d3dbe9676da783f87222c248
MeeXaSiK <87555873+MeeXaSiK@users.noreply.github.com>
2022-07-31 16:49:16 +0700

    Update Singleton.cs

--- Code/System/Singleton.cs
+++ Code/System/Singleton.cs
@@ -1,7 +1,7 @@
 ﻿// ----------------------------------------------------------------
 // The MIT License
 // Singleton for Unity https://github.com/MeeXaSiK/NightSingleton
-// Copyright (c) 2021 Night Train Code
+// Copyright (c) 2021-2022 Night Train Code
 // ----------------------------------------------------------------
 
 using UnityEngine;
@@ -11,44 +11,49 @@ namespace NTC.Global.System
     public class Singleton<TSingleton> : MonoBehaviour where TSingleton : MonoBehaviour
     {
         public static TSingleton Instance => GetNotNull();
+
         private static TSingleton cachedInstance;
 
         public static TSingleton GetCanBeNull()
+        {
+            return GetInstance(true);
+        }
+
+        public static TSingleton GetNotNull()
+        {
+            return GetInstance(false);
+        }
+
+        private static TSingleton GetInstance(bool canBeNull)
         {
             if (cachedInstance != null)
             {
                 return cachedInstance;
             }
             
-            var instances = FindObjectsOfType<TSingleton>();
-            var instance = instances.Length > 0 ? instances[0] : null;
-
-            for (var i = 1; i < instances.Length; i++)
+            var allInstances = FindObjectsOfType<TSingleton>();
+            var instance = allInstances.Length > 0
+                ? allInstances[0]
+                : GetInstanceIfNotFound(canBeNull);
+            
+            if (allInstances.Length > 1)
             {
-                Destroy(instances[i]);
+                Debug.LogError($"The number of {typeof(TSingleton).Name} on the scene is greater than one!");
+            }
+            
+            for (var i = 1; i < allInstances.Length; i++)
+            {
+                Destroy(allInstances[i]);
             }
 
             return cachedInstance = instance;
         }
 
-        public static TSingleton GetNotNull()
+        private static TSingleton GetInstanceIfNotFound(bool canBeNull)
         {
-            if (cachedInstance != null)
-            {
-                return cachedInstance;
-            }
-            
-            var instances = FindObjectsOfType<TSingleton>();
-            var instance = instances.Length > 0 
-                ? instances[0] 
+            return canBeNull 
+                ? null 
                 : new GameObject($"[Singleton] {typeof(TSingleton).Name}").AddComponent<TSingleton>();
-
-            for (var i = 1; i < instances.Length; i++)
-            {
-                Destroy(instances[i]);
-            }
-
-            return cachedInstance = instance;
         }
     }
 }
