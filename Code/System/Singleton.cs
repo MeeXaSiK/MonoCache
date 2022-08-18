a0179533c8b1e584a3eab8ab88c9c40a1909afab
MeeXaSiK <87555873+MeeXaSiK@users.noreply.github.com>
2022-08-19 00:22:03 +0700

    Update Singleton.cs

--- Code/System/Singleton.cs
+++ Code/System/Singleton.cs
@@ -8,22 +8,54 @@ using UnityEngine;
 
 namespace NTC.Global.System
 {
-    public class Singleton<TSingleton> : MonoBehaviour where TSingleton : MonoBehaviour
+    public abstract class Singleton<TSingleton> : MonoBehaviour where TSingleton : MonoBehaviour
     {
-        public static TSingleton Instance => GetNotNull();
+        /// <summary>
+        /// Returns a target instance
+        /// </summary>
+        public static TSingleton Instance
+        {
+            get
+            {
+                lock (SecurityLock)
+                {
+                    return GetInstance(false);
+                }
+            }
+        }
 
+        /// <summary>
+        /// Returns the target instance, ignoring thread safety
+        /// </summary>
+        public static TSingleton InstanceNonLock => GetInstance(false);
+
+        /// <summary>
+        /// Can return a null instance if target one not found
+        /// </summary>
+        /// <returns> Target instance or null if not found </returns>
+        public static TSingleton GetCanBeNull() => GetInstance(true);
+
+        /// <summary>
+        /// Will create the new instance if target one not found
+        /// </summary>
+        /// <returns> Target instance </returns>
+        public static TSingleton GetNotNull() => GetInstance(false);
+        
+        /// <summary>
+        /// Field for cached instance
+        /// </summary>
         private static TSingleton cachedInstance;
+        
+        /// <summary>
+        /// Object for thread safety
+        /// </summary>
+        private static readonly object SecurityLock = new object();
 
-        public static TSingleton GetCanBeNull()
-        {
-            return GetInstance(true);
-        }
-
-        public static TSingleton GetNotNull()
-        {
-            return GetInstance(false);
-        }
-
+        /// <summary>
+        /// Main method to get target instance
+        /// </summary>
+        /// <param name="canBeNull"> Can return a nullable instance </param>
+        /// <returns> Target instance </returns>
         private static TSingleton GetInstance(bool canBeNull)
         {
             if (cachedInstance != null)
@@ -38,17 +70,23 @@ namespace NTC.Global.System
             
             if (allInstances.Length > 1)
             {
+                for (var i = 1; i < allInstances.Length; i++)
+                {
+                    Destroy(allInstances[i]);
+                }
+#if DEBUG
                 Debug.LogError($"The number of {typeof(TSingleton).Name} on the scene is greater than one!");
-            }
-            
-            for (var i = 1; i < allInstances.Length; i++)
-            {
-                Destroy(allInstances[i]);
+#endif
             }
 
             return cachedInstance = instance;
         }
 
+        /// <summary>
+        /// Decides what to do if the instance is not found
+        /// </summary>
+        /// <param name="canBeNull"> Can return a nullable instance if true </param>
+        /// <returns> Null or new instance </returns>
         private static TSingleton GetInstanceIfNotFound(bool canBeNull)
         {
             return canBeNull 
