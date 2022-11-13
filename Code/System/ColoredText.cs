25151e0415a054cd6ec197e57a40eb4d5bd7b335
RimuruDev <Rimuru.Dev@gmail.com>
2022-11-13 14:54:41 +0600

    Removed extra spaces.

--- Code/System/ColoredText.cs
+++ Code/System/ColoredText.cs
@@ -6,7 +6,7 @@
         public const string BlueColor = "00FFF7";
         public const string OrangeColor = "F4CA16";
         public const string RedColor = "E22121";
-        
+
         public static string GetColoredText(string color, string text)
         {
             if (color.IndexOf('#') == -1)
