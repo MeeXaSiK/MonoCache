be2ca4e8fb8d148858f512a8792c56480b83612d
RimuruDev <Rimuru.Dev@gmail.com>
2022-11-13 14:52:57 +0600

    Code reduction [MonoAllocation.cs].
    1) Removed extra spaces.
    2) Added indentation for the return statement of the GetSingleCached() method.
    3) The body of the methods has been shortened thanks to the lambda operator.

--- Code/System/MonoAllocation.cs
+++ Code/System/MonoAllocation.cs
@@ -19,73 +19,42 @@ namespace NTC.Global.System
 
         private bool allocationEnabled = true;
 
-        public void EnableAllocation()
-        {
-            allocationEnabled = true;
-        }
+        public void EnableAllocation() => allocationEnabled = true;
 
-        public void DisableAllocation()
-        {
-            allocationEnabled = false;
-        }
+        public void DisableAllocation() => allocationEnabled = false;
 
         public T Get<T>() => GetComponent<T>();
-        
+
         public T[] Gets<T>() => GetComponents<T>();
-        
+
         public T ChildrenGet<T>() => GetComponentInChildren<T>();
-        
+
         public T[] ChildrenGets<T>() => GetComponentsInChildren<T>();
-        
+
         public T ParentGet<T>() => GetComponentInParent<T>();
-        
+
         public T[] ParentGets<T>() => GetComponentsInParent<T>();
-        
+
         public T Find<T>() where T : Object => FindObjectOfType<T>();
-        
+
         public T[] Finds<T>() where T : Object => FindObjectsOfType<T>();
-        
 
-        public T GetCached<T>() where T : Component
-        {
-            return GetSingleCached(_get, GetComponent<T>);
-        }
+        public T GetCached<T>() where T : Component => GetSingleCached(_get, GetComponent<T>);
 
-        public T[] GetsCached<T>() where T : Component
-        {
-            return GetManyCached(_gets, GetComponents<T>);
-        }
+        public T[] GetsCached<T>() where T : Component => GetManyCached(_gets, GetComponents<T>);
 
-        public T ChildrenGetCached<T>() where T : Component
-        {
-            return GetSingleCached(_childrenGet, GetComponentInChildren<T>);
-        }
+        public T ChildrenGetCached<T>() where T : Component => GetSingleCached(_childrenGet, GetComponentInChildren<T>);
 
-        public T[] ChildrenGetsCached<T>() where T : Component
-        {
-            return GetManyCached(_childrenGets, GetComponentsInChildren<T>);
-        }
+        public T[] ChildrenGetsCached<T>() where T : Component => GetManyCached(_childrenGets, GetComponentsInChildren<T>);
 
-        public T ParentGetCached<T>() where T : Component
-        {
-            return GetSingleCached(_parentGet, GetComponentInParent<T>);
-        }
+        public T ParentGetCached<T>() where T : Component => GetSingleCached(_parentGet, GetComponentInParent<T>);
 
-        public T[] ParentGetsCached<T>() where T : Component
-        {
-            return GetManyCached(_parentGets, GetComponentsInParent<T>);
-        }
+        public T[] ParentGetsCached<T>() where T : Component => GetManyCached(_parentGets, GetComponentsInParent<T>);
 
-        public T FindCached<T>() where T : Component
-        {
-            return GetSingleCached(_find, FindObjectOfType<T>);
-        }
+        public T FindCached<T>() where T : Component => GetSingleCached(_find, FindObjectOfType<T>);
 
-        public T[] FindsCached<T>() where T : Component
-        {
-            return GetManyCached(_finds, FindObjectsOfType<T>);
-        }
-        
+        public T[] FindsCached<T>() where T : Component => GetManyCached(_finds, FindObjectsOfType<T>);
+
         private T GetSingleCached<T>(Dictionary<int, Component> storage, Func<T> getMethod) where T : Component
         {
             var index = GetInfo<T>.Index;
@@ -96,7 +65,7 @@ namespace NTC.Global.System
 
                 if (storage.TryGetValue(index, out var component))
                 {
-                    return (T) component;
+                    return (T)component;
                 }
             }
 
@@ -120,7 +89,7 @@ namespace NTC.Global.System
 
                 if (storage.TryGetValue(index, out var components))
                 {
-                    return (T[]) components;
+                    return (T[])components;
                 }
             }
 
